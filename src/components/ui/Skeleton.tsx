cc8933c342150611b6c89debf08355b72cefdd4a
p6remant <paudelremant6@gmail.com>
2026-08-04 15:13:10 +0545

    feat: create skeleton.

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -1,1 +1,14 @@
-// ## Skeleton UI Component
+import { HTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
+
+export function Skeleton({
+  className = "",
+  ...props
+}: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("bg-app-muted animate-pulse rounded", className)}
+      {...props}
+    />
+  );
+}
